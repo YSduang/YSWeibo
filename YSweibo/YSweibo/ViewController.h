aaa76506fec115aa63a05ffb39f898c1c8607be5
YSduang <z13520@live.com>
2015-04-18 19:40:59 +0800

    初始化了项目

---
+++ YSweibo/YSweibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YSweibo
+//
+//  Created by Brent-mac on 15/4/18.
+//  Copyright (c) 2015年 brent－mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
